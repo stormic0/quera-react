2d4a571b4622e6958e1bad13db2e04a9f082bbee
Amin Payamfar <aminpayamfar@Amins-MacBook-Pro.local>
2022-03-20 16:09:13 +0330

    Season 6 project 3 final state added

--- 05 Season6 - Hooks & Events/03 useToggle-Hook/src/App.jsx
+++ 05 Season6 - Hooks & Events/03 useToggle-Hook/src/App.jsx
@@ -4,12 +4,13 @@ import "react-toggle/style.css";
 import { useToggle } from "./Hook";
 
 function App() {
+  const [state, toggle] = useToggle();
 
   return (
     <div className="App">
       <label>
-        <Toggle />
-        <span>Toggle</span>
+        <Toggle onChange={toggle} />
+        <span>{state ? "Toggled" : "Toggle"}</span>
       </label>
     </div>
   );
--- 05 Season6 - Hooks & Events/03 useToggle-Hook/src/Hook.js
+++ 05 Season6 - Hooks & Events/03 useToggle-Hook/src/Hook.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
+
 const useToggle = (initialValue) => {
-  const state = initialValue;
-  const toggle = () => null;
+  const [state, setState] = useState(initialValue);
+
+  const toggle = () => setState(!state);
 
   return [state, toggle];
+};
+
+useToggle.defaultProps = {
+  initialValue: false,
 };
 
 export { useToggle };
