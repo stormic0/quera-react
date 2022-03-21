e85133eb3aae263ae869b4c51f46714303bc494a
Amin Payamfar <aminpayamfar@Amins-MacBook-Pro.local>
2022-03-21 23:17:28 +0330

    Chapter 10 project 3 final state added

--- 09 Chapter10 - APIs/03 quera-chat/src/components/MessageForm/MessageForm.js
+++ 09 Chapter10 - APIs/03 quera-chat/src/components/MessageForm/MessageForm.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import './MessageForm.css'
+import React, { useState, useRef, useEffect } from "react";
+import "./MessageForm.css";
 
 function MessageForm({ onMessageSend }) {
-  function handleFormSubmit(event) {}
+  const [textInput, setTextInput] = useState("");
+  const focusOnInput = useRef(null);
+
+  const handleInputChange = (event) => {
+    setTextInput(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (textInput.length > 0 && (textInput.match(/ /g) || []).length !== textInput.length) {
+      onMessageSend({ me: true, body: textInput });
+      setTextInput("");
+    }
+  };
+
+  useEffect(() => {
+    focusOnInput.current.focus();
+  }, []);
+
   return (
-    <form
-      className="MessageForm"
-      onSubmit={handleFormSubmit}
-      data-testid="submit-message"
-    >
+    <form className="MessageForm" onSubmit={handleFormSubmit} data-testid="submit-message">
       <div className="input-container">
         <input
           data-testid="input-message"
           type="text"
           placeholder="پیام خود را اینجا بنویسید..."
+          onChange={handleInputChange}
+          value={textInput}
+          ref={focusOnInput}
         />
       </div>
       <div className="button-container">
         <button type="submit">ارسال</button>
       </div>
     </form>
-  )
+  );
 }
 
-export default MessageForm
+export default MessageForm;
--- 09 Chapter10 - APIs/03 quera-chat/src/containers/App.js
+++ 09 Chapter10 - APIs/03 quera-chat/src/containers/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import MessageList from '../components/MessageList/MessageList'
-import MessageForm from '../components/MessageForm/MessageForm'
-import './App.css'
+import React, { useState } from "react";
+import MessageList from "../components/MessageList/MessageList";
+import MessageForm from "../components/MessageForm/MessageForm";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>
+  const [messages, setMessages] = useState([]);
+
+  const getResponse = async (message) => {
+    const res = await fetch(`http://localhost:3001/message/${message}`);
+    const data = await res.json();
+    setMessages((messages) => [...messages, { me: false, body: data.message }]);
+  };
+
+  const onSendMessage = (message) => {
+    setMessages([...messages, message]);
+    getResponse(message.body);
+  };
+
+  return (
+    <div className="App">
+      <MessageList messages={messages} />
+      <MessageForm onMessageSend={onSendMessage} />
+    </div>
+  );
 }
 
-export default App
+export default App;
