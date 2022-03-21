bd477aeb6088b0e37a802d462c153bdc58b24c6c
Amin Payamfar <aminpayamfar@Amins-MacBook-Pro.local>
2022-03-21 09:03:22 +0330

    Season 7 project 3 final state added

--- 06 Season7 - Forms/03 input-validation/src/components/Input.jsx
+++ 06 Season7 - Forms/03 input-validation/src/components/Input.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import { validate } from '../utils/validators'
-
-const INPUT_STATES = {
-  UNTOUCHED: 'UNTOUCHED',
-  VALID: 'VALID',
-  INVALID: 'INVALID',
-}
+import React, { useState } from "react";
+import { validate } from "../utils/validators";
 
 const Input = ({ label, id, type, validators, errorText }) => {
-  return (
-    <div className="form-input" data-testid="form-input">
-      <label></label>
-      <input />
-      <p></p>
-    </div>
-  )
-}
+  const [firstTime, setFirstTime] = useState(true);
+  const [isValid, setIsValid] = useState(true);
 
-export default Input
+  const handleFocusOut = (e) => {
+    setFirstTime(false);
+    setIsValid(validate(e.target.value, validators));
+  };
+
+  return (
+    <div
+      className={`form-input ${
+        !firstTime && !isValid && "form-input--invalid"
+      }`}
+      data-testid="form-input"
+    >
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        onBlur={handleFocusOut}
+        onChange={(e) => setIsValid(validate(e.target.value, validators))}
+      />
+      <p>{!firstTime && !isValid && errorText}</p>
+    </div>
+  );
+};
+
+export default Input;
