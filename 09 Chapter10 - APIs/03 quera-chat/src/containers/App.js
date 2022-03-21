7efd31ceea9f4c74fc479cf7ab2c4113ff7edec7
Amin Payamfar <aminpayamfar@Amins-MacBook-Pro.local>
2022-03-21 23:10:26 +0330

    Chapter 10 project 3 initial state added

---
+++ 09 Chapter10 - APIs/03 quera-chat/src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MessageList from '../components/MessageList/MessageList'
+import MessageForm from '../components/MessageForm/MessageForm'
+import './App.css'
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App
